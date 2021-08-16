c1eb56ac960223d5b98a4751e4d35ed704d61753
Edson Moretti do Nascimento <edsonmoretti@users.noreply.github.com>
2021-08-16 16:04:47 -0300

    Delete configuration.php

--- admin/app/config/configuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost:3313');
-define('DB_USER', 'cursoPhp7.4');
-define('DB_PASSWORD', 'cursoPhp7.4');
-define('DB_NAME', 'lojinha');
-define('SESSION_NAME', md5('loggedUser' . date('dmy'))); //isso me dá um timeout de 24h
-
-define('URL', 'http://localhost:8013/admin/');
